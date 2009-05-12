39dcb6fb6834c99813f9c4b78e4e734ecd2d4753
Brad Anderson <brad@sankatygroup.com>
2009-05-12 00:18:03 -0400

    fixing defines for testing debugMsg and debugFmt macros

--- include/common.hrl
+++ include/common.hrl
@@ -4,6 +4,6 @@
 -define(infoMsg(Msg), error_logger:info_msg(Msg)).
 
 -ifndef(TEST).
--define(?debugMsg(Msg), ok).
--define(?debugFmt(Msg, Args), ok).
+-define(debugMsg(Msg), ok).
+-define(debugFmt(Msg, Args), ok).
 -endif.
