e0181c9e36266c98bf9705383efc1bf3546ace44
Brad Anderson <brad@sankatygroup.com>
2009-05-14 14:59:43 -0400

    adding ability to get distinct attributes from node metadata, will be used in replication algo

--- elibs/replication.erl
+++ elibs/replication.erl
@@ -34,7 +34,31 @@
 %%--------------------------------------------------------------------
 partners(Node, Nodes, Config) ->
   ?debugFmt("~nNode  : ~p~nNodes : ~p~nConfig: ~p~n", [Node, Nodes, Config]),
+  _Distinct = distinct_attributes(Nodes),
   [].
+
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+distinct_attributes(Nodes) ->
+  Attribs = lists:map(fun node:attributes/1, Nodes),
+  ?debugFmt("~nAttribs : ~p~n", [Attribs]),
+  Distinct = distinct({[], Attribs}),
+  ?debugFmt("~nDistinct: ~p~n", [Distinct]),
+  Distinct.
+
+distinct({Result, done}) ->
+  lists:reverse(Result);
+distinct({Acc, Attribs}) ->
+  Tails = lists:map(fun([_H|T]) -> T end, Attribs),
+  %% if any of the attribute lists is exhausted, we are done, throw out the rest
+  NewTails = case lists:member([], Tails) of
+               true -> done;
+               false -> Tails
+             end,
+  Heads = lists:foldl(fun([H|_T], AccIn) -> [H | AccIn] end, [], Attribs),
+  NewHeads = lists:usort(Heads),
+%%   ?debugFmt("~nNewHeadsAcc: ~p~nNewTails   : ~p~n",
+%%             [[NewHeads | Acc], NewTails]),
+  distinct({[NewHeads | Acc], NewTails}).
